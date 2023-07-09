82135a7dbc9862f7d24b4a2d4f5f0227e5b68b5b
Sinskiy <sinskiy@proton.me>
2023-07-09 15:15:14 +0300

    refactor: move architecture in function and run function every second & on page load

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 // DF SoundCloud
 
-// individual
-// select
-const logo = document.querySelector(".header__logoLink");
-const rightMenu = document.querySelector(".header__right");
+function main() {
+    // individual
+    // select
+    const rightMenu = document.querySelector(".header__right");
 
-//delete
-logo.style.display = "none";
-rightMenu.style.display = "none";
+    //delete
+    // i need to use this crazy tertiary operator because logo can change during the usage
+    document.querySelector(".header__logoLink")
+     ? document.querySelector(".header__logoLink").style.display = "none"
+     : document.querySelector(".peace-logo .header__logoLink").style.display = "none";
 
-// multiple
-// select
-const everyLeftLink = document.querySelectorAll("a.header__navMenuItem");
-const dontDelete = "a[data-menu-name=library]";
+    rightMenu.style.display = "none";
 
-// delete
-everyLeftLink.forEach((link) => {
-    if (link !== document.querySelector(dontDelete)) {
-        link.style.display = "none";
-    }
-})
+    // multiple
+    // select
+    const everyLeftLink = document.querySelectorAll("a.header__navMenuItem");
+    const dontDelete = "a[data-menu-name=library]";
+
+    // delete
+    everyLeftLink.forEach((link) => {
+        if (link !== document.querySelector(dontDelete)) {
+            link.style.display = "none";
+        }
+    });
+}
+
+// run on page load
+main();
+
+//run every 1s
+const checkHeader = setInterval(() => {
+    main();
+}, 1000);
